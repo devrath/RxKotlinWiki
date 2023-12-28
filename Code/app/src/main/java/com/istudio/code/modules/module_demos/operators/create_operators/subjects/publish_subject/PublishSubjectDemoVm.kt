e6b1f91f1e993d8dfb557189261d2ee3d2a4476a
Devrath <devrath.dev595@gmail.com>
2023-12-28 20:35:35 +0530

    Added data

--- Code/app/src/main/java/com/istudio/code/modules/module_demos/operators/create_operators/subjects/publish_subject/PublishSubjectDemoVm.kt
+++ Code/app/src/main/java/com/istudio/code/modules/module_demos/operators/create_operators/subjects/publish_subject/PublishSubjectDemoVm.kt
@@ -3,13 +3,59 @@ package com.istudio.code.modules.module_demos.operators.create_operators.subject
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import episodeI
+import episodeII
+import episodeIII
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.subjects.PublishSubject
 import javax.inject.Inject
 
 @HiltViewModel
 class PublishSubjectDemoVm @Inject constructor(context: Application) : AndroidViewModel(context) {
-    fun demo() {
 
+    private val subject = PublishSubject.create<String>()
+    private val disposable = CompositeDisposable()
+
+
+    fun demo() {
+        // Subscriber - 1
+        subject.subscribeBy(
+            onNext = { println("Subscriber-1:-> $it") },
+            onError = { println("Subscriber-1:-> Error") },
+            onComplete = { println("Subscriber-1:-> Complete") }
+        ).addTo(disposable)
+
+        subject.onNext(episodeI)
+
+        // Subscriber - 2
+        subject.subscribeBy(
+            onNext = { println("Subscriber-2:-> $it") },
+            onError = { println("Subscriber-2:-> Error") },
+            onComplete = { println("Subscriber-2:-> Complete") }
+        ).addTo(disposable)
+
+        subject.onNext(episodeII)
+
+        // Subscriber - 3
+        subject.subscribeBy(
+            onNext = { println("Subscriber-3:-> $it") },
+            onError = { println("Subscriber-3:-> Error") },
+            onComplete = { println("Subscriber-3:-> Complete") }
+        ).addTo(disposable)
+
+        // Indicate onComplete of the publish subject
+        subject.onComplete()
+
+        // Try to publish a new onNext event
+        subject.onNext(episodeIII)
     }
 
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 
 }
